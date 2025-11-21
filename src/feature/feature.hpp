ce662c428007301ac947cba56cf00377c92a4365
K10-K10 <keito.tottori@gmail.com>
2025-11-21 15:38:04 +0900

    format

--- src/feature/feature.hpp
+++ src/feature/feature.hpp
@@ -2,24 +2,22 @@
 #ifndef TERMINAL_LIBRARY_FEATURE_
 #define TERMINAL_LIBRARY_FEATURE_
 #include <iostream>
-#include <string>
 #include <istream>
-namespace T_feature
-{
-	void PasswordInput(std::string &Password)
-	{
-		char c;
-		// while (std::cin.get(c))
-		//{
-		//	Password += c;
-		//	std::cout << "\x80 \x80";
-		//	std::cout << '*' << std::flush;
-		//	if (c == '\n')
-		//	{
-		//		break;
-		//	}
-		// }
-		}
+#include <string>
+namespace T_feature {
+void PasswordInput(std::string &Password) {
+  char c;
+  // while (std::cin.get(c))
+  //{
+  //	Password += c;
+  //	std::cout << "\x80 \x80";
+  //	std::cout << '*' << std::flush;
+  //	if (c == '\n')
+  //	{
+  //		break;
+  //	}
+  // }
 }
+} // namespace T_feature
 
 #endif TERMINAL_LIBRARY_FEATURE_
