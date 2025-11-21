fce6e40263f4acdb842172bd079421bfdd70f3e5
K10-K10 <keito.tottori@gmail.com>
2025-11-21 15:45:27 +0900

    add:get terminal size()

--- src/base/base.hpp
+++ src/base/base.hpp
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 namespace T_base {
 
@@ -27,6 +29,21 @@ inline void init_cursor() { std::cout << "\x1b[2J\x1b[H" << std::flush; }
 
 inline void move_cursor(int x, int h) {
   std::cout << "\033[" << x << ";" << h << "H" << std::flush;
+}
+
+inline int get_terminal_columns() {
+  struct winsize w;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != -1)
+    return w.ws_col;
+  else
+    return -1;
+}
+inline int get_terminal_rows() {
+  struct winsize w;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != -1)
+    return w.ws_row;
+  else
+    return -1;
 }
 
 } // namespace T_base
