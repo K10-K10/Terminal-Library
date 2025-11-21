36f8b7f24391799aa39380170b08d327606ae9e5
K10-K10 <keito.tottori@gmail.com>
2025-11-21 15:37:54 +0900

    refact

--- src/base/base.hpp
+++ src/base/base.hpp
@@ -2,16 +2,14 @@
 
 #ifndef TERMINAL_LIBRARY_BASE_
 #define TERMINAL_LIBRARY_BASE_
+
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 namespace T_base {
-void BackSpace(int letter_num) {
-  for (int i = 0; i < letter_num; i++) {
-    std::cout << "\x08 \x08" << std::flush;
-  }
-};
-void cls() {
+
+inline void cls() {
 #ifdef _WIN32
   system("cls");
 #else
@@ -19,6 +17,18 @@ void cls() {
 #endif
 }
 
+inline void BackSpace(int letter_num) {
+  for (int i = 0; i < letter_num; i++) {
+    std::cout << "\x08 \x08" << std::flush;
+  }
+}
+
+inline void init_cursor() { std::cout << "\x1b[2J\x1b[H" << std::flush; }
+
+inline void move_cursor(int x, int h) {
+  std::cout << "\033[" << x << ";" << h << "H" << std::flush;
+}
+
 } // namespace T_base
 
-#endif TERMINAL_LIBRARY_BASE_
+#endif // TERMINAL_LIBRARY_BASE_
