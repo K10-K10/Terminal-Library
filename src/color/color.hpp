bae62e75b0030eed80b0ecdbad7cf8e8f74f43cd
K10-K10 <keito.tottori@gmail.com>
2025-11-22 16:40:59 +0900

    add:color

---
+++ src/color/color.hpp
@@ -0,0 +1,19 @@
+#ifndef __color
+#define __color
+
+#define text_red 31
+#define text_green 32
+#define text_yellow 33
+#define text_blue 34
+#define text_magenta 35
+#define text_sian 36
+#define text_gray 37
+#define fill_red 41
+#define fill_green 42
+#define fill_yellow 43
+#define fill_blue 44
+#define fill_magenta 45
+#define fill_sian 46
+#define fill_gray 46
+
+#endif
