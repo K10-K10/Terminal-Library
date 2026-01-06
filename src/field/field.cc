767a05cec031205e37883bdb79990c1838b0fbac
K10-K10 <keito.tottori@gmail.com>
2026-01-07 00:15:08 +0900

    add: field obj exit func

--- src/field/field.cc
+++ src/field/field.cc
@@ -20,7 +20,12 @@ void Field::init() {
       terminal_manager::obj_drawing, std::ref(terminal_manager::running));
 }
 
-void Field::exit() { hide(); }
+void Field::exit() {
+  hide();
+  terminal_manager::running.store(false);
+  if (terminal_manager::draw_thread.joinable())
+    terminal_manager::draw_thread.join();
+}
 
 void Field::show() {
   for (const auto& pair : field_map) {
@@ -29,7 +34,7 @@ void Field::show() {
   }
 }
 
-void Field::hide() {
+void Field::hide() {  // TODO(K10-K10): doesn't work
   for (const auto& pair : field_map) {
     Object* obj = pair.second;
     terminal_manager::obj_map[obj].show = false;
