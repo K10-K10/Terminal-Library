0cdc86d9a91bfb61bd06b4d9bd1c6550e22fac17
K10-K10 <keito.tottori@gmail.com>
2026-04-08 16:03:07 +0100

    feat: add block rendering method

--- src/obj/block.cc
+++ src/obj/block.cc
@@ -1,6 +1,7 @@
 #include "obj/block.h"
 
-#include "obj/obj.h"
+#include "core/drawObj.h"
+#include "layout/rect.h"
 
 namespace terminal {
 Block& Block::set_type(int new_type) {
@@ -10,6 +11,19 @@ Block& Block::set_type(int new_type) {
 
 int Block::get_type() { return type_; }
 
-void Block::draw(Screen& s, const Rect& r) {}
+void Block::draw(const Rect& r) {
+  __terminal__::drawObj.put(r.y, r.x, {*single.tl});
+  __terminal__::drawObj.put(r.y, r.right(), {*single.tr});
+  __terminal__::drawObj.put(r.bottom(), r.x, {*single.bl});
+  __terminal__::drawObj.put(r.bottom(), r.right(), {*single.br});
+  for (int x = r.x + 1; x < r.right(); ++x) {
+    __terminal__::drawObj.put(r.y, x, {*single.h});
+    __terminal__::drawObj.put(r.bottom(), x, {*single.h});
+  }
+  for (int y = r.y + 1; y < r.bottom(); ++y) {
+    __terminal__::drawObj.put(y, r.x, {*single.v});
+    __terminal__::drawObj.put(y, r.right(), {*single.v});
+  }
+}
 
 }  // namespace terminal
--- src/obj/block.h
+++ src/obj/block.h
@@ -3,12 +3,27 @@
 namespace terminal {
 class Block : public Object {
  public:
-  void draw(Screen& s, const Rect& r) override;
+  void draw(const Rect& r) override;
   Block& set_type(int new_type);
   int get_type();
 
  private:
   int type_;
+  struct Border {
+    const char* tl;  // top-left
+    const char* tr;
+    const char* bl;
+    const char* br;
+
+    const char* h;  // horizontal
+    const char* v;  // vertical
+  };
+
+  const Border single{"┌", "┐", "└", "┘", "─", "│"};
+
+  const Border bold{"┏", "┓", "┗", "┛", "━", "┃"};
+
+  const Border double_line{"╔", "╗", "╚", "╝", "═", "║"};
 };
 
 }  // namespace terminal
