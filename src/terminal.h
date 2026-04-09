fb90ab89eebf09f4b2019aae8d56713a5deb1a84
K10-K10 <keito.tottori@gmail.com>
2026-04-09 15:27:51 +0100

    feat: update build, include files

--- src/terminal.h
+++ src/terminal.h
@@ -1,12 +1,19 @@
-#pragma once
-
 #ifndef LIBRARY_TERMINAL_TERMINAL_H_
 #define LIBRARY_TERMINAL_TERMINAL_H_
 
 #include "feat/feature.h"
 #include "input/cursor.h"
 #include "input/keyboard.h"
-#include "utils/base.h"
+// #include "utils/base.h"
+#include "app/app.h"
+#include "layout/rect.h"
+#include "obj/block.h"
+#include "obj/list.h"
+#include "obj/text.h"
 #include "utils/color.h"
+
+namespace terminal {
+__terminal__::App app;
+}
 
 #endif  // LIBRARY_TERMINAL_TERMINAL_H_
