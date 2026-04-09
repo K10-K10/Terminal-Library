73bf61d62ee8213595398dbc8403412d230248d7
K10-K10 <keito.tottori@gmail.com>
2026-04-09 15:31:50 +0100

    fix: modified namespce

--- src/core/render.cc
+++ src/core/render.cc
@@ -1,3 +1,6 @@
+#ifndef CORE_RENDER_CC_
+#define CORE_RENDER_CC_
+
 #include "core/render.h"
 
 #include <iostream>
@@ -8,8 +11,8 @@
 
 namespace __terminal__ {
 void Render::flush() {
-  int W = screen.width;
-  int H = screen.height;
+  int W = terminal::screen.width();
+  int H = terminal::screen.height();
 
   for (int y = 0; y < H; ++y) {
     int start = -1;
@@ -18,18 +21,19 @@ void Render::flush() {
     for (int x = 0; x < W; ++x) {
       int i = y * W + x;
 
-      if (screen.current[i].c != screen.next[i].c) {
+      if (terminal::screen.current[i].c != terminal::screen.next[i].c) {
         if (start == -1) {
           start = x;
           line.clear();
         }
 
-        line += screen.next[i].c;
+        line += terminal::screen.next[i].c;
       } else if (start != -1) {
         terminal::utils::moveTo(y, start);
         std::cout << line;
         for (int k = 0; k < line.size(); ++k) {
-          screen.current[y * W + start + k] = screen.next[y * W + start + k];
+          terminal::screen.current[y * W + start + k] =
+              terminal::screen.next[y * W + start + k];
         }
         start = -1;
       }
@@ -39,7 +43,8 @@ void Render::flush() {
       std::cout << line;
 
       for (int k = 0; k < line.size(); ++k) {
-        screen.current[y * W + start + k] = screen.next[y * W + start + k];
+        terminal::screen.current[y * W + start + k] =
+            terminal::screen.next[y * W + start + k];
       }
     }
   }
@@ -49,3 +54,5 @@ void Render::flush() {
 Render render;
 
 }  // namespace __terminal__
+
+#endif
