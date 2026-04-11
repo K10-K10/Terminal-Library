fe03869b335332578e1ebaab785df9439df3b104
K10-K10 <keito.tottori@gmail.com>
2026-04-11 16:27:56 +0100

    refactor: delete debug

--- src/core/render.cc
+++ src/core/render.cc
@@ -7,13 +7,10 @@
 #include "core/screen.h"
 #include "utils/base.h"
 
-std::ofstream logs("log.txt", std::ios::app);
-
 namespace __terminal__ {
 void Render::flush() {
   int W = terminal::screen.width();
   int H = terminal::screen.height();
-  logs << "--- FLUSH START ---" << std::endl;
   int start = -1;
   std::string line;
   for (int y = 0; y < H; ++y) {
@@ -33,8 +30,6 @@ void Render::flush() {
       } else if (start != -1) {
         terminal::utils::moveTo(y, start);
         std::cout << line;
-        logs << "Update at (" << y << ", " << start << "): " << line
-             << std::endl;
         start = -1;
         line.clear();
       }
@@ -42,15 +37,12 @@ void Render::flush() {
     if (start != -1) {
       terminal::utils::moveTo(y, start);
       std::cout << line << std::flush;
-      logs << "Update at (" << y << ", " << start << "): " << line << std::endl;
       start = -1;
       line.clear();
     }
   }
   terminal::screen.current = terminal::screen.next;
   terminal::screen.next.clear();
-  logs << "--- FLUSH END ---" << std::endl;
-  logs.flush();
   std::cout << std::flush;
 }
 
