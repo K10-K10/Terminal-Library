0454ce06122f300082262da4ce7ce5fef7232dab
K10-K10 <keito.tottori@gmail.com>
2026-04-11 12:58:05 +0100

    refactor: block draw func

--- src/obj/block.cc
+++ src/obj/block.cc
@@ -17,17 +17,26 @@ Block& Block::set_type(int new_type) {
 int Block::get_type() { return type_; }
 
 void Block::draw() {
-  __terminal__::drawObj.put(rect.y, rect.x, {single.tl});
-  __terminal__::drawObj.put(rect.y, rect.right(), {single.tr});
-  __terminal__::drawObj.put(rect.bottom(), rect.x, {single.bl});
-  __terminal__::drawObj.put(rect.bottom(), rect.right(), {single.br});
-  for (int x = rect.x + 1; x < rect.right(); ++x) {
-    __terminal__::drawObj.put(rect.y, x, {single.h});
-    __terminal__::drawObj.put(rect.bottom(), x, {single.h});
+  if (rect.w < 2 || rect.h < 2) return;
+
+  int l = rect.x;
+  int r = rect.right();
+  int t = rect.y;
+  int b = rect.bottom();
+
+  __terminal__::drawObj.put(t, l, {single.tl});
+  __terminal__::drawObj.put(t, r, {single.tr});
+  __terminal__::drawObj.put(b, l, {single.bl});
+  __terminal__::drawObj.put(b, r, {single.br});
+
+  for (int x = l + 1; x < r; ++x) {
+    __terminal__::drawObj.put(t, x, {single.h});
+    __terminal__::drawObj.put(b, x, {single.h});
   }
-  for (int y = rect.y + 1; y < rect.bottom(); ++y) {
-    __terminal__::drawObj.put(y, rect.x, {single.v});
-    __terminal__::drawObj.put(y, rect.right(), {single.v});
+
+  for (int y = t + 1; y < b; ++y) {
+    __terminal__::drawObj.put(y, l, {single.v});
+    __terminal__::drawObj.put(y, r, {single.v});
   }
 }
 
